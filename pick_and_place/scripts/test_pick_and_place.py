63b34b25f49ddf6107d787e34c2a52932e1b77a4
JaniC-WPI <schatterjee@wpi.edu>
2022-04-19 23:37:27 -0400

    add camera transform publisher launch

--- pick_and_place/scripts/test_pick_and_place.py
+++ pick_and_place/scripts/test_pick_and_place.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 from pick_and_place_module.pick_and_place import PickAndPlace
@@ -6,18 +6,18 @@ from std_msgs.msg import Float64
 from math import pi
 
 def pick_and_place():
-    pick_and_place = PickAndPlace(gripper_offset=0.05, intermediate_z_stop=0.5)
+    pick_and_place = PickAndPlace(gripper_offset=0.05, intermediate_z_stop=0.6)
     
     pick_and_place.setDropPose(x=0.0, y=0.4, z=0.4, roll=0, pitch=pi, yaw=0)
-    pick_and_place.setPickPose(x=0.50, y=0.00, z=0.12, roll=0*3.14/180, pitch=pi, yaw=0)
-    pick_and_place.setGripperPose(width=0.1)
+    pick_and_place.setPickPose(x=0.30, y=0.00, z=0.2, roll=0, pitch=pi, yaw=0)
+    pick_and_place.setGripperPose(width=0.0)
     
     # pick_and_place.execute_pick_and_place()
-    # pick_and_place.execute_pick_up()
+    pick_and_place.execute_pick_up()
     # pick_and_place.execute_place()
     # pick_and_place.execute_cartesian_pick_and_place()
-    pick_and_place.execute_cartesian_pick_up()
-    pick_and_place.execute_cartesian_place()
+    # pick_and_place.execute_cartesian_pick_up()
+    # pick_and_place.execute_cartesian_place()
 
 if __name__ == "__main__":
     rospy.init_node("test_pnp")
